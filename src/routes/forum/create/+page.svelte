59b8692e604b5e37b96157fff46b6b8f1a2ca35b
blekekok <jtbernas@gmail.com>
2024-07-09 21:26:39 +0700

    Update +page.svelte

--- src/routes/forum/create/+page.svelte
+++ src/routes/forum/create/+page.svelte
@@ -58,7 +58,8 @@
     <form method="POST" on:submit|preventDefault={handleSubmit} class="flex flex-col w-full bg-slate-900 rounded-xl p-6 gap-6">
         <input id="title" bind:value={title} class="input h-10 px-4" type="text" placeholder="Title">
 
-        <Editor bind:value={content} licenseKey="gpl" />
+        <!-- <Editor bind:value={content} licenseKey="gpl" /> -->
+	<textarea bind:value={content}></textarea>
 
         <InputChip bind:value={tags} name="chips" placeholder="Enter your tags here" />
 
